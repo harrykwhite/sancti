7999b16052bf81011e3c2e5daec3de057dfa296d
Harry White <harrywhiteproductions@gmail.com>
2024-12-14 11:28:55 +1000

    Add simple enemy spawning and rendering.

--- src/game.cpp
+++ src/game.cpp
@@ -2,12 +2,24 @@
 
 Game i_game;
 
+static void spawn_enemy(const zf3::Vec2D pos) {
+    const int enemyIndex = zf3::get_first_inactive_bit_index(i_game.enemyActivity.bytes, gk_enemyLimit);
+
+    if (enemyIndex == -1) {
+        zf3::log_error("Failed to spawn enemy as all enemy slots are taken!");
+        return;
+    }
+
+    zf3::activate_bit(i_game.enemyActivity.bytes, enemyIndex);
+    i_game.enemies[enemyIndex].pos = pos;
+}
+
 void init_game(const zf3::UserGameFuncData* const zf3Data) {
     zf3Data->renderer->bgColor = {0.59f, 0.79f, 0.93f, 1.0f};
     zf3::load_render_layers(zf3Data->renderer, RENDER_LAYER_CNT, UI_RENDER_LAYER);
 
-    //i_game.player.pos.x = zf3Data->windowMeta->size.x / 2.0f;
-    //i_game.player.pos.y = zf3Data->windowMeta->size.y / 2.0f;
+    i_game.player.pos.x = zf3Data->windowMeta->size.x / 2.0f;
+    i_game.player.pos.y = zf3Data->windowMeta->size.y / 2.0f;
 
     zf3Data->cam->scale = 2.0f;
 }
@@ -27,11 +39,45 @@ void run_game_tick(const zf3::UserGameFuncData* const zf3Data) {
         i_game.player.pos.x += moveAxis.x * moveSpd;
         i_game.player.pos.y += moveAxis.y * moveSpd;
 
-        //zf3Data->cam->pos = i_game.player.pos;
+        zf3Data->cam->pos = i_game.player.pos;
 
         const zf3::SpriteBatchWriteData writeData = {
             .texIndex = 0,
             .pos = i_game.player.pos,
+            .srcRect = {0, 0, 24, 40},
+            .origin = {0.5f, 0.5f},
+            .rot = 0.0f,
+            .scale = {1.0f, 1.0f},
+            .alpha = 1.0f
+        };
+
+        zf3::write_to_sprite_batch(zf3Data->renderer, WORLD_RENDER_LAYER, &writeData, zf3Data->assets);
+    }
+
+    //
+    // Enemies
+    //
+    if (i_game.enemySpawnTime > 0) {
+        --i_game.enemySpawnTime;
+    } else {
+        const zf3::Vec2D spawnPos = {
+            zf3::gen_rand_float(0.0f, static_cast<float>(zf3Data->windowMeta->size.x)),
+            zf3::gen_rand_float(0.0f, static_cast<float>(zf3Data->windowMeta->size.y))
+        };
+
+        spawn_enemy(spawnPos);
+
+        i_game.enemySpawnTime = gk_enemySpawnInterval;
+    }
+
+    for (int i = 0; i < gk_enemyLimit; ++i) {
+        if (!zf3::is_bit_active(i_game.enemyActivity.bytes, i)) {
+            continue;
+        }
+
+        const zf3::SpriteBatchWriteData writeData = {
+            .texIndex = 0,
+            .pos = i_game.enemies[i].pos,
             .srcRect = {0, 0, 24, 40},
             .origin = {0.5f, 0.5f},
             .rot = 0.0f,
--- src/game.h
+++ src/game.h
@@ -2,6 +2,9 @@
 
 #include <zf3.h>
 
+constexpr int gk_enemyLimit = 64;
+constexpr int gk_enemySpawnInterval = 120;
+
 enum RenderLayer {
     WORLD_RENDER_LAYER,
     UI_RENDER_LAYER,
@@ -13,8 +16,16 @@ struct Player {
     zf3::Vec2D pos;
 };
 
+struct Enemy {
+    zf3::Vec2D pos;
+};
+
 struct Game {
     Player player;
+
+    Enemy enemies[gk_enemyLimit];
+    zf3::Bitset<gk_enemyLimit> enemyActivity;
+    int enemySpawnTime;
 };
 
 void init_game(const zf3::UserGameFuncData* const zf3Data);
